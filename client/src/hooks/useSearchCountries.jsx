ddb5a5c04312acef5bda9f7d27693c3944a19cf4
santiagoweb212 <juanusca45@gmail.com>
2023-06-08 10:53:05 -0500

    bug reset option solucionado

--- client/src/hooks/useSearchCountries.jsx
+++ client/src/hooks/useSearchCountries.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react";
 import { fecthData } from "../redux/actions/fetchData";
 import { useDispatch } from "react-redux";
 import { getCurrentPage } from "../redux/actions/getCurrentPage";
+import { useMenuOptionSelect } from "./useMenuOptionSelect";
 
 export const useSearchCountries=()=>{
-
+const {handleDeleteFilterClick}=useMenuOptionSelect()
     const [valueSearch, setValueSearch] = useState("");
     const dispatch = useDispatch();
     const handleSearChange = (e) => {
         const value = e.target.value;
         setValueSearch(value);
         dispatch(getCurrentPage('1'))
+        handleDeleteFilterClick('reset')
       };
     const handleSearchClick = (e) => {
         dispatch(
